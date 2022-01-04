e986cbd4549d342a818492935a9e7e69b41d5259
kalipoison <1174616074@qq.com>
2022-01-05 00:19:54 +0800

    首页Price Service

--- front/src/components/home/Price.vue
+++ front/src/components/home/Price.vue
@@ -1,19 +1,126 @@
 <template>
     <div id="content">
         <div id="content_text">
-            <span>PRICING</span><br>
-            <b>Simple and Transparent Pricing</b><br>
-            <span>All type plans have been designed to fit most people need.</span>
-        </div>
-        <div id="content_card">
-            <b>One account to rule them all</b><br>
-            <span>Connect from any platform using one same account.</span>
-            <div>
-                <div class="card card1">1</div>
-                <div class="card card2">1</div>
-                <div class="card card3">1</div>
-                <div class="nocard"></div>
-            </div>
+            <el-row><h2>PRICING</h2></el-row>
+            <el-row><b>Simple and Transparent Pricing</b></el-row>
+            <el-row><span>All type plans have been designed to fit most people need.</span></el-row>
+            <el-row :style="{padding:'50px'}">
+                <el-col :span="6" :offset="1">
+                    <el-card :body-style="{ padding: '0px' }">
+                    <div>
+                        <div class="card_header">
+                            <el-row :style="{padding:'15px'}">
+                                <span>Basic<br></span>
+                            </el-row>
+                            <el-row :style="{padding:'5px'}">
+                                <span>$1.6/month</span>
+                            </el-row>
+                        </div>
+                        <div class="card_content clearfix">
+                            <el-row :style="{padding:'15px'}">
+                              <el-col :span="4"><img src="../../assets/gou.png" width="25px" height="25px"/></el-col>
+                              <el-col :span="20">
+                                  <span>3 Devices</span><br>
+                                  <span>active simultaneously</span>
+                              </el-col>
+                            </el-row>
+                            <el-row :style="{padding:'15px'}">
+                              <el-col :span="4"><img src="../../assets/gou.png" width="25px" height="25px"/></el-col>
+                              <el-col :span="20">
+                                  <span>5 Datacenters</span><br>
+                                  <span>100+ servers pool</span>
+                              </el-col>
+                            </el-row>
+                            <el-row :style="{padding:'15px'}">
+                              <el-col :span="4"><img src="../../assets/gou.png" width="25px" height="25px"/></el-col>
+                              <el-col :span="20">
+                                  <span>3 Devices</span><br>
+                                  <span>active simultaneously</span>
+                              </el-col>
+                            </el-row>
+                            <el-button round>Start Today</el-button>
+                        </div>
+                    </div>
+                    </el-card>
+                </el-col>
+                <el-col :span="6" :offset="2">
+                    <el-card :body-style="{ padding: '0px' }">
+                    <div>
+                        <div class="card_header">
+                            <el-row :style="{padding:'15px'}">
+                                <span>Pro<br></span>
+                            </el-row>
+                            <el-row :style="{padding:'5px'}">
+                                <span>$4.2/month</span>
+                            </el-row>
+                        </div>
+                        <div class="card_content clearfix">
+                            <el-row :style="{padding:'15px'}">
+                              <el-col :span="4"><img src="../../assets/gou.png" width="25px" height="25px"/></el-col>
+                              <el-col :span="20">
+                                  <span>3 Devices</span><br>
+                                  <span>active simultaneously</span>
+                              </el-col>
+                            </el-row>
+                            <el-row :style="{padding:'15px'}">
+                              <el-col :span="4"><img src="../../assets/gou.png" width="25px" height="25px"/></el-col>
+                              <el-col :span="20">
+                                  <span>5 Datacenters</span><br>
+                                  <span>100+ servers pool</span>
+                              </el-col>
+                            </el-row>
+                            <el-row :style="{padding:'15px'}">
+                              <el-col :span="4"><img src="../../assets/gou.png" width="25px" height="25px"/></el-col>
+                              <el-col :span="20">
+                                  <span>3 Devices</span><br>
+                                  <span>active simultaneously</span>
+                              </el-col>
+                            </el-row>
+                            <el-button round>Start Today</el-button>
+                        </div>
+                    </div>
+                    </el-card>
+                </el-col>
+                <el-col :span="6" :offset="2">
+                    <el-card :body-style="{ padding: '0px' }">
+                    <div>
+                        <div class="card_header">
+                            <el-row :style="{padding:'15px'}">
+                                <span>Team<br></span>
+                            </el-row>
+                            <el-row :style="{padding:'5px'}">
+                                <span>$18/month</span>
+                            </el-row>
+                        </div>
+                        <div class="card_content clearfix">
+                            <el-row :style="{padding:'15px'}">
+                              <el-col :span="4"><img src="../../assets/gou.png" width="25px" height="25px"/></el-col>
+                              <el-col :span="20">
+                                  <span>3 Devices</span><br>
+                                  <span>active simultaneously</span>
+                              </el-col>
+                            </el-row>
+                            <el-row :style="{padding:'15px'}">
+                              <el-col :span="4"><img src="../../assets/gou.png" width="25px" height="25px"/></el-col>
+                              <el-col :span="20">
+                                  <span>5 Datacenters</span><br>
+                                  <span>100+ servers pool</span>
+                              </el-col>
+                            </el-row>
+                            <el-row :style="{padding:'15px'}">
+                              <el-col :span="4"><img src="../../assets/gou.png" width="25px" height="25px"/></el-col>
+                              <el-col :span="20">
+                                  <span>3 Devices</span><br>
+                                  <span>active simultaneously</span>
+                              </el-col>
+                            </el-row>
+                            <el-button round>Start Today</el-button>
+                        </div>
+                    </div>
+                    </el-card>
+                </el-col>
+
+            </el-row>
         </div>
     </div>
 </template>
@@ -22,37 +129,58 @@
 #content {
     text-align: center;
     background-color: #3182ce;
-    padding: 50px;
+    padding-bottom: 10%;
 }
-#content_text {
-    b {
-        font-size: 36px;
-        color: #fff;
-    }
+h2 {
+    color: #e2e8f0;
+    font-size: 18px;
+}
+b {
+    font-size: 36px;
+    color: #fff;
+}
+span {
+    font-size: 24px;
+    color: #e2e8f0;
+}
+
+.el-card {
+    height: 500px;
+}
+
+.el-col {
     span {
-        font-size: 24px;
-        color: #e2e8f0;
+        font-size: 16px;
+        color: #a0aec0;
     }
 }
-#content_card {
-    div {
-        padding-left: 15%;
-        background-color: #fff;
-        .card {
-            float: left;
-            padding: 50px;
-        }
-        .nocard {
-            clear: both;
-        }
-        .card1 .card3 {
-            width: 20%;
-            height: 80px;
-        }
-        .card2 {
-            width: 20%;
-            height: 80px;
-        }
+
+.el-button {
+    margin: 100px;
+}
+
+.card_header {
+    height: 100px;
+    text-align: center;
+    background: #fff;
+    span {
+        color: #1a202c;
     }
+}
+.card_content {
+    height: 400px;
+    background: #edf2f7;
+    color: #a0a2a0;
+    font-size: 16px;
+}
+
+.clearfix:before,
+.clearfix:after {
+    display: table;
+    content: "";
+}
+
+.clearfix:after {
+    clear: both
 }
 </style>
--- front/src/components/home/Service.vue
+++ front/src/components/home/Service.vue
@@ -1,5 +1,178 @@
 <template>
-    <div>About</div>
+    <div id="content">
+        <div id="content_text">
+            <el-row><h2>PROTOCOLS</h2></el-row>
+            <el-row><b>Flexible Solutions</b></el-row>
+            <el-row><span>You can access GLaDOS edge network via V2Ray or Wireguard clients.
+                <br>Switch based on your needs anytime.</span></el-row>
+            <el-row :style="{padding:'50px'}">
+                <el-col :span="10" :offset="2">
+                    <el-card :body-style="{ padding: '0px' }">
+                    <div>
+                        <div class="card_header">
+                            <el-row :style="{padding:'15px'}">
+                                <span>Basic<br></span>
+                            </el-row>
+                            <el-row :style="{padding:'5px'}">
+                                <span>$1.6/month</span>
+                            </el-row>
+                        </div>
+                        <div class="card_content clearfix">
+                            <el-row :style="{padding:'15px'}">
+                              <el-col :span="4"><img src="../../assets/gou.png" width="25px" height="25px"/></el-col>
+                              <el-col :span="20">
+                                  <span>Mainstream, Best for web and productivity</span>
+                              </el-col>
+                            </el-row>
+                            <el-row :style="{padding:'15px'}">
+                              <el-col :span="4"><img src="../../assets/gou.png" width="25px" height="25px"/></el-col>
+                              <el-col :span="20">
+                                  <span>Split Traffic base on Rules.</span>
+                              </el-col>
+                            </el-row>
+                            <el-row :style="{padding:'15px'}">
+                              <el-col :span="4"><img src="../../assets/gou.png" width="25px" height="25px"/></el-col>
+                              <el-col :span="20">
+                                  <span>Auto selecting best endpoints</span>
+                              </el-col>
+                            </el-row>
+                            <el-row :style="{padding:'15px'}">
+                              <el-col :span="4"><img src="../../assets/gou.png" width="25px" height="25px"/></el-col>
+                              <el-col :span="20">
+                                  <span>For Windows, MacOS, iOS and Android.</span>
+                              </el-col>
+                            </el-row>
+                            <el-row :style="{padding:'15px'}">
+                              <el-col :span="4"><img src="../../assets/gou.png" width="25px" height="25px"/></el-col>
+                              <el-col :span="20">
+                                  <span>Config file subscription</span>
+                              </el-col>
+                            </el-row>
+                            <el-row :style="{padding:'15px'}">
+                              <el-col :span="4"><img src="../../assets/gou.png" width="25px" height="25px"/></el-col>
+                              <el-col :span="20">
+                                  <span>Optimized for speed</span>
+                              </el-col>
+                            </el-row>
+                            <el-button round>Start Today</el-button>
+                        </div>
+                    </div>
+                    </el-card>
+                </el-col>
+                <el-col :span="10" :offset="2">
+                    <el-card :body-style="{ padding: '0px' }">
+                    <div>
+                        <div class="card_header">
+                            <el-row :style="{padding:'15px'}">
+                                <span>Basic<br></span>
+                            </el-row>
+                            <el-row :style="{padding:'5px'}">
+                                <span>$1.6/month</span>
+                            </el-row>
+                        </div>
+                        <div class="card_content clearfix">
+                            <el-row :style="{padding:'15px'}">
+                              <el-col :span="4"><img src="../../assets/gou.png" width="25px" height="25px"/></el-col>
+                              <el-col :span="20">
+                                  <span>Backup solution</span>
+                              </el-col>
+                            </el-row>
+                            <el-row :style="{padding:'15px'}">
+                              <el-col :span="4"><img src="../../assets/gou.png" width="25px" height="25px"/></el-col>
+                              <el-col :span="20">
+                                  <span>Best for Gaming and Conference</span>
+                              </el-col>
+                            </el-row>
+                            <el-row :style="{padding:'15px'}">
+                              <el-col :span="4"><img src="../../assets/gou.png" width="25px" height="25px"/></el-col>
+                              <el-col :span="20">
+                                  <span>Full Traffic Routing</span>
+                              </el-col>
+                            </el-row>
+                            <el-row :style="{padding:'15px'}">
+                              <el-col :span="4"><img src="../../assets/gou.png" width="25px" height="25px"/></el-col>
+                              <el-col :span="20">
+                                  <span>Operating at OSI Layer 3</span>
+                              </el-col>
+                            </el-row>
+                            <el-row :style="{padding:'15px'}">
+                              <el-col :span="4"><img src="../../assets/gou.png" width="25px" height="25px"/></el-col>
+                              <el-col :span="20">
+                                  <span>Game, Full ICMP/UDP support</span>
+                              </el-col>
+                            </el-row>
+                            <el-row :style="{padding:'15px'}">
+                              <el-col :span="4"><img src="../../assets/gou.png" width="25px" height="25px"/></el-col>
+                              <el-col :span="20">
+                                  <span>Fixed Datacenter binding</span>
+                              </el-col>
+                            </el-row>
+                            <el-button round>Start Today</el-button>
+                        </div>
+                    </div>
+                    </el-card>
+                </el-col>
+            </el-row>
+        </div>
+    </div>
 </template>
 
+<style lang="less" scoped>
+#content {
+    text-align: center;
+    background-color: #3182ce;
+    padding-bottom: 10%;
+}
+h2 {
+    color: #e2e8f0;
+    font-size: 18px;
+}
+b {
+    font-size: 36px;
+    color: #fff;
+}
+span {
+    font-size: 24px;
+    color: #e2e8f0;
+}
 
+.el-card {
+    height: 600px;
+}
+
+.el-col {
+    span {
+        font-size: 16px;
+        color: #a0aec0;
+    }
+}
+
+.el-button {
+    // margin: 100px;
+}
+
+.card_header {
+    height: 100px;
+    text-align: center;
+    background: #fff;
+    span {
+        color: #1a202c;
+    }
+}
+.card_content {
+    height: 500px;
+    background: #edf2f7;
+    color: #a0a2a0;
+    font-size: 16px;
+}
+
+.clearfix:before,
+.clearfix:after {
+    display: table;
+    content: "";
+}
+
+.clearfix:after {
+    clear: both
+}
+</style>
