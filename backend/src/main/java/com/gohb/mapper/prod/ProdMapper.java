731835700e98f53b70adc0624821a2bc995cc534
kalipoison <1174616074@qq.com>
2022-01-12 21:48:44 +0800

    Prod 漏提交文件

---
+++ backend/src/main/java/com/gohb/mapper/prod/ProdMapper.java
@@ -0,0 +1,18 @@
+package com.gohb.mapper.prod;
+
+import com.gohb.params.bo.prod.ProdBO;
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+
+/**
+* @author zsh
+* @description 针对表【prod(商品)】的数据库操作Mapper
+* @createDate 2022-01-12 01:33:21
+* @Entity com.gohb.generate.domain.Prod
+*/
+public interface ProdMapper extends BaseMapper<ProdBO> {
+
+}
+
+
+
+
---
+++ backend/src/main/java/com/gohb/service/prod/ProdService.java
@@ -0,0 +1,13 @@
+package com.gohb.service.prod;
+
+import com.gohb.params.bo.prod.ProdBO;
+import com.baomidou.mybatisplus.extension.service.IService;
+
+/**
+* @author zsh
+* @description 针对表【prod(商品)】的数据库操作Service
+* @createDate 2022-01-12 01:33:21
+*/
+public interface ProdService extends IService<ProdBO> {
+
+}
