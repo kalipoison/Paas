d59775b13f67913d9218a496991e8f66cd379016
kalipoison <1174616074@qq.com>
2022-01-14 00:37:39 +0800

    Voucher 兑换券漏提交 文件

---
+++ backend/src/main/java/com/gohb/mapper/voucher/VoucherMapper.java
@@ -0,0 +1,18 @@
+package com.gohb.mapper.voucher;
+
+import com.gohb.params.bo.voucher.VoucherBO;
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+
+/**
+* @author zsh
+* @description 针对表【voucher(兑换券表)】的数据库操作Mapper
+* @createDate 2022-01-12 22:53:30
+* @Entity com.gohb.params.bo.Voucher.Voucher
+*/
+public interface VoucherMapper extends BaseMapper<VoucherBO> {
+
+}
+
+
+
+
---
+++ backend/src/main/java/com/gohb/params/bo/voucher/VoucherBO.java
@@ -0,0 +1,106 @@
+package com.gohb.params.bo.voucher;
+
+import com.baomidou.mybatisplus.annotation.IdType;
+import com.baomidou.mybatisplus.annotation.TableField;
+import com.baomidou.mybatisplus.annotation.TableId;
+import com.baomidou.mybatisplus.annotation.TableName;
+import java.io.Serializable;
+import java.util.Date;
+import lombok.Data;
+
+/**
+ * 兑换券表
+ * @TableName voucher
+ */
+@TableName(value ="voucher")
+@Data
+public class VoucherBO implements Serializable {
+    /**
+     * 兑换券ID
+     */
+    @TableId(value = "voucher_id", type = IdType.AUTO)
+    private Long voucherId;
+
+    /**
+     * 使用用户id
+     */
+    @TableField(value = "user_id")
+    private Long userId;
+
+    /**
+     * 兑换码
+     */
+    @TableField(value = "voucher_code")
+    private String voucherCode;
+
+    /**
+     * 创建时间
+     */
+    @TableField(value = "create_time")
+    private Date createTime;
+
+    /**
+     * 过期时间
+     */
+    @TableField(value = "expire_time")
+    private Date expireTime;
+
+    /**
+     * 状态  0：未使用   1：已使用
+     */
+    @TableField(value = "status")
+    private Byte status;
+
+    @TableField(exist = false)
+    private static final long serialVersionUID = 1L;
+
+    @Override
+    public boolean equals(Object that) {
+        if (this == that) {
+            return true;
+        }
+        if (that == null) {
+            return false;
+        }
+        if (getClass() != that.getClass()) {
+            return false;
+        }
+        VoucherBO other = (VoucherBO) that;
+        return (this.getVoucherId() == null ? other.getVoucherId() == null : this.getVoucherId().equals(other.getVoucherId()))
+            && (this.getUserId() == null ? other.getUserId() == null : this.getUserId().equals(other.getUserId()))
+            && (this.getVoucherCode() == null ? other.getVoucherCode() == null : this.getVoucherCode().equals(other.getVoucherCode()))
+            && (this.getCreateTime() == null ? other.getCreateTime() == null : this.getCreateTime().equals(other.getCreateTime()))
+            && (this.getExpireTime() == null ? other.getExpireTime() == null : this.getExpireTime().equals(other.getExpireTime()))
+            && (this.getStatus() == null ? other.getStatus() == null : this.getStatus().equals(other.getStatus()));
+    }
+
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + ((getVoucherId() == null) ? 0 : getVoucherId().hashCode());
+        result = prime * result + ((getUserId() == null) ? 0 : getUserId().hashCode());
+        result = prime * result + ((getVoucherCode() == null) ? 0 : getVoucherCode().hashCode());
+        result = prime * result + ((getCreateTime() == null) ? 0 : getCreateTime().hashCode());
+        result = prime * result + ((getExpireTime() == null) ? 0 : getExpireTime().hashCode());
+        result = prime * result + ((getStatus() == null) ? 0 : getStatus().hashCode());
+        return result;
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+        sb.append(getClass().getSimpleName());
+        sb.append(" [");
+        sb.append("Hash = ").append(hashCode());
+        sb.append(", voucherId=").append(voucherId);
+        sb.append(", userId=").append(userId);
+        sb.append(", voucherCode=").append(voucherCode);
+        sb.append(", createTime=").append(createTime);
+        sb.append(", expireTime=").append(expireTime);
+        sb.append(", status=").append(status);
+        sb.append(", serialVersionUID=").append(serialVersionUID);
+        sb.append("]");
+        return sb.toString();
+    }
+}
