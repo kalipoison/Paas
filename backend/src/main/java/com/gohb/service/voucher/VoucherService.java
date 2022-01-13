7e9d1ad80d74c4323cfd719e5969c2dac5c32de1
kalipoison <1174616074@qq.com>
2022-01-14 00:37:39 +0800

    Voucher 兑换券漏提交 文件

---
+++ backend/src/main/java/com/gohb/service/voucher/VoucherService.java
@@ -0,0 +1,13 @@
+package com.gohb.service.voucher;
+
+import com.gohb.params.bo.voucher.VoucherBO;
+import com.baomidou.mybatisplus.extension.service.IService;
+
+/**
+* @author zsh
+* @description 针对表【voucher(兑换券表)】的数据库操作Service
+* @createDate 2022-01-12 22:53:30
+*/
+public interface VoucherService extends IService<VoucherBO> {
+
+}
---
+++ backend/src/main/java/com/gohb/service/voucher/impl/VoucherServiceImpl.java
@@ -0,0 +1,22 @@
+package com.gohb.service.voucher.impl;
+
+import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import com.gohb.params.bo.voucher.VoucherBO;
+import com.gohb.service.voucher.VoucherService;
+import com.gohb.mapper.voucher.VoucherMapper;
+import org.springframework.stereotype.Service;
+
+/**
+* @author zsh
+* @description 针对表【voucher(兑换券表)】的数据库操作Service实现
+* @createDate 2022-01-12 22:53:30
+*/
+@Service
+public class VoucherServiceImpl extends ServiceImpl<VoucherMapper, VoucherBO>
+    implements VoucherService{
+
+}
+
+
+
+
