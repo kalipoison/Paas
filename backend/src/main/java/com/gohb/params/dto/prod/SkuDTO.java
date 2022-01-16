b45a596fdb78c3708c169d3b8182715c7289de9c
kalipoison <1174616074@qq.com>
2022-01-16 21:35:19 +0800

    Sku 变更表结构
    (cherry picked from commit 419defca5605a577a37276f753fed3390ecaf255)

--- backend/src/main/java/com/gohb/params/dto/prod/SkuDTO.java
+++ backend/src/main/java/com/gohb/params/dto/prod/SkuDTO.java
@@ -1,79 +1,47 @@
 package com.gohb.params.dto.prod;
 
-import com.baomidou.mybatisplus.annotation.IdType;
-import com.baomidou.mybatisplus.annotation.TableField;
-import com.baomidou.mybatisplus.annotation.TableId;
-import com.baomidou.mybatisplus.annotation.TableName;
 import lombok.Data;
 
 import java.io.Serializable;
 
-/**
- * 单品SKU表
- * @TableName sku
- */
-@TableName(value ="sku")
+
 @Data
 public class SkuDTO implements Serializable {
     /**
      * 单品ID
      */
-    @TableId(value = "sku_id", type = IdType.AUTO)
     private Long skuId;
 
     /**
      * 商品ID
      */
-    @TableField(value = "prod_id")
     private Long prodId;
 
     /**
-     * 属性组合字符串 格式是p1:v1;p2:v2
+     * 商品名
      */
-    @TableField(value = "properties")
-    private String properties;
+    private String productName;
 
-    @TableField(exist = false)
+    /**
+     * 属性值ID
+     */
+    private Long valueId;
+
+    /**
+     * 属性值
+     */
+    private String propValue;
+
+    /**
+     * 属性ID
+     */
+    private Long propId;
+
+    /**
+     * 属性名
+     */
+    private String propName;
+
     private static final long serialVersionUID = 1L;
 
-    @Override
-    public boolean equals(Object that) {
-        if (this == that) {
-            return true;
-        }
-        if (that == null) {
-            return false;
-        }
-        if (getClass() != that.getClass()) {
-            return false;
-        }
-        SkuDTO other = (SkuDTO) that;
-        return (this.getSkuId() == null ? other.getSkuId() == null : this.getSkuId().equals(other.getSkuId()))
-            && (this.getProdId() == null ? other.getProdId() == null : this.getProdId().equals(other.getProdId()))
-            && (this.getProperties() == null ? other.getProperties() == null : this.getProperties().equals(other.getProperties()));
-    }
-
-    @Override
-    public int hashCode() {
-        final int prime = 31;
-        int result = 1;
-        result = prime * result + ((getSkuId() == null) ? 0 : getSkuId().hashCode());
-        result = prime * result + ((getProdId() == null) ? 0 : getProdId().hashCode());
-        result = prime * result + ((getProperties() == null) ? 0 : getProperties().hashCode());
-        return result;
-    }
-
-    @Override
-    public String toString() {
-        StringBuilder sb = new StringBuilder();
-        sb.append(getClass().getSimpleName());
-        sb.append(" [");
-        sb.append("Hash = ").append(hashCode());
-        sb.append(", skuId=").append(skuId);
-        sb.append(", prodId=").append(prodId);
-        sb.append(", properties=").append(properties);
-        sb.append(", serialVersionUID=").append(serialVersionUID);
-        sb.append("]");
-        return sb.toString();
-    }
 }
