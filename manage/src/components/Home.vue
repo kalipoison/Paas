ed78111d3b286bfc791fae427d752fefb87e0f36
kalipoison <1174616074@qq.com>
2022-01-18 14:37:01 +0800

    修复 后台首页 路由BUG

--- manage/src/components/Home.vue
+++ manage/src/components/Home.vue
@@ -19,7 +19,7 @@
           :collapse-transition="false"
           router
         >
-          <el-menu-item index="home">
+          <el-menu-item index="/home">
             <i class="el-icon-menu"></i>
             <span slot="title">首页</span>
           </el-menu-item>
