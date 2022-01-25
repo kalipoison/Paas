783e450e93521ffddcc89d1de7ade15adb73b8c9
kalipoison <1174616074@qq.com>
2022-01-25 23:33:30 +0800

    新增springApplicationContextHolder工具类，获取bean工厂

---
+++ backend/src/main/java/com/gohb/utils/ApplicationContextHolder.java
@@ -0,0 +1,33 @@
+package com.gohb.utils;
+
+import org.springframework.beans.BeansException;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ApplicationContextAware;
+import org.springframework.stereotype.Component;
+
+/**
+ * 获取bean工厂
+ */
+@Component
+public class ApplicationContextHolder implements ApplicationContextAware {
+
+    /**
+     * 上下文对象实例
+     */
+    private static ApplicationContext applicationContext;
+
+    @Override
+    public void setApplicationContext(ApplicationContext applicationContext) throws BeansException {
+        this.applicationContext = applicationContext;
+    }
+
+    /**
+     * 获取applicationContext
+     *
+     * @return
+     */
+    public static ApplicationContext getApplicationContext() {
+        return applicationContext;
+    }
+
+}
