d9482b7e48130fd8d73ed6d459637c6211f94e49
kalipoison <1174616074@qq.com>
2022-01-28 02:52:39 +0800

    Jedis 与 RedisTemplate性能对比 测试

---
+++ backend/src/test/java/com/gohb/test/GreExam.java
@@ -0,0 +1,14 @@
+package com.gohb.test;
+
+public class GreExam {
+
+    private Integer id;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+}
