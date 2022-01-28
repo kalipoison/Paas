577c72e2a6acef0dac868915e8d74358bfaf381c
kalipoison <1174616074@qq.com>
2022-01-29 00:31:33 +0800

    rabbitmq 实现 延迟队列

---
+++ backend/src/main/java/com/gohb/config/mq/MqOrderConfig.java
@@ -0,0 +1,48 @@
+package com.gohb.config.mq;
+
+
+import com.gohb.constant.QueueConstant;
+import org.springframework.amqp.core.Binding;
+import org.springframework.amqp.core.BindingBuilder;
+import org.springframework.amqp.core.DirectExchange;
+import org.springframework.amqp.core.Queue;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.HashMap;
+import java.util.Map;
+
+
+@Configuration
+public class MqOrderConfig {
+
+    @Bean
+    public Queue orderMsQueue() {
+        Map<String, Object> args = new HashMap<>();
+        // 设置这个队列里面的消息 死亡后 走哪个交换机
+        args.put("x-dead-letter-exchange", QueueConstant.ORDER_DEAD_EX);
+        // 设置这个队列里面的消息 死亡后 走哪个路由key
+        args.put("x-dead-letter-routing-key", QueueConstant.ORDER_DEAD_KEY);
+        // 设置这个队列里面的消息的死亡时间 三分钟时间
+        args.put("x-message-ttl", 180 * 1000);
+        return new Queue(QueueConstant.ORDER_MS_QUEUE, true, false, false, args);
+    }
+
+    @Bean
+    public Queue orderDeadQueue() {
+        return new Queue(QueueConstant.ORDER_DEAD_QUEUE);
+    }
+
+    @Bean
+    public DirectExchange orderDeadEx() {
+        return new DirectExchange(QueueConstant.ORDER_DEAD_EX);
+    }
+
+    @Bean
+    public Binding orderDeadBing() {
+        return BindingBuilder.bind(orderDeadQueue()).to(orderDeadEx()).with(QueueConstant.ORDER_DEAD_KEY);
+    }
+
+
+}
+
