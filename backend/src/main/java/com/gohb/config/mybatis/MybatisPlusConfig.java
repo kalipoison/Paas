ffb55390c3709c5bc5e6eb0ed5dcfe4147927671
kalipoison <1174616074@qq.com>
2022-01-30 00:21:04 +0800

    mybatis 多数据源，读写分离，master为主库，负责增删改；slave_1、slave_2为从库，负责查询。

--- backend/src/main/java/com/gohb/config/mybatis/MybatisPlusConfig.java
+++ backend/src/main/java/com/gohb/config/mybatis/MybatisPlusConfig.java
@@ -1,14 +1,18 @@
 package com.gohb.config.mybatis;
 
+import com.baomidou.dynamic.datasource.plugin.MasterSlaveAutoRoutingPlugin;
 import com.baomidou.mybatisplus.annotation.DbType;
 import com.baomidou.mybatisplus.autoconfigure.ConfigurationCustomizer;
 import com.baomidou.mybatisplus.extension.plugins.MybatisPlusInterceptor;
+import com.baomidou.mybatisplus.extension.plugins.OptimisticLockerInterceptor;
 import com.baomidou.mybatisplus.extension.plugins.inner.PaginationInnerInterceptor;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @Configuration
+@EnableTransactionManagement
 @MapperScan(basePackages = {"com.gohb.mapper"})
 public class MybatisPlusConfig {
 
@@ -23,6 +27,24 @@ public class MybatisPlusConfig {
     @Bean
     public ConfigurationCustomizer configurationCustomizer() {
         return configuration -> configuration.setUseDeprecatedExecutor(false);
+    }
+
+    /**
+     * 乐观锁配置
+     * @return
+     */
+    @Bean
+    public OptimisticLockerInterceptor optimisticLockerInterceptor() {
+        return new OptimisticLockerInterceptor();
+    }
+
+    /**
+     * 读写分离配置
+     * @return
+     */
+    @Bean
+    public MasterSlaveAutoRoutingPlugin masterSlaveAutoRoutingPlugin() {
+        return new MasterSlaveAutoRoutingPlugin();
     }
 
 }
